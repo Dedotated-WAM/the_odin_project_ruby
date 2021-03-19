cbce38693a52f4409ca1730dd520ed8f5296aae3
Hokie007 <13981130+Hokie007@users.noreply.github.com>
2021-03-19 15:47:48 -0400

    Complete caesar_cipher

---
+++ caesar_cipher/caesar_cipher.rb
@@ -0,0 +1,17 @@
+# Takes a string and alpha character shift value, outputting ciphered string
+class CaesarCipher
+  def translate(string, shift)
+    result = ''
+    string.each_char do |char|
+      base = char.ord < 91 ? 65 : 97
+      if char.ord.between?(65, 90) || char.ord.between?(97, 122)
+        result << ((((char.ord - base) + shift) % 26) + base).chr
+      else
+        result += char
+      end
+    end
+    result
+  end
+end
+
+p CaesarCipher.new.translate('What a string!', 5)
