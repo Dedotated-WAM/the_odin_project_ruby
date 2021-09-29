708e60776d28bf8cdedae2ead95e7bcdc3c10d78
Hokie007 <13981130+Hokie007@users.noreply.github.com>
2021-09-29 13:29:13 -0400

    Fix game exit bug

--- tdd_connect_four/lib/game.rb
+++ tdd_connect_four/lib/game.rb
@@ -7,8 +7,8 @@ module ConnectFour
       @maximum = maximum
       @current_move = current_move
       @round_number = 0
-      @player_one = Player.new('Player One', BLACK_CIRCLE)
-      @player_two = Player.new('Player Two', RED_CIRCLE)
+      @player_one = Player.new("Player One", BLACK_CIRCLE)
+      @player_two = Player.new("Player Two", RED_CIRCLE)
       @players = [@player_one, @player_two]
       @current_player, @other_player = @players.shuffle
       @board = Board.new
@@ -49,14 +49,14 @@ module ConnectFour
 
     def play_again?
       loop do
-        puts 'Play again? (y/n)'
+        puts "Play again? (y/n)"
         input = gets.chomp
-        if input.downcase.split('').include?('y')
+        if input.downcase.split("").include?("y")
           return true
-        elsif input.downcase.split('').include?('n')
+        elsif input.downcase.split("").include?("n")
           return false
         else
-          puts 'Invalid entry'
+          puts "Invalid entry"
           redo
         end
       end
@@ -74,9 +74,9 @@ module ConnectFour
           return nil if play_again?
 
           begin
-            puts 'Exiting Program'
-            puts 'Thanks for playing! Goodbye.'
-            exit
+            puts "Exiting Program"
+            puts "Thanks for playing! Goodbye."
+            exit!
           rescue SystemExit
             return nil
           end
